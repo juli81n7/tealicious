031692aa5e3eddf26e4ace0ac0f2beb762bafb8c
juli81n7 <112468707+juli81n7@users.noreply.github.com>
2022-12-09 13:26:22 +0100

    ændret i menu, mangler stadigt lidt

--- faelles.js
+++ faelles.js
@@ -1,28 +1,27 @@
-// Lav variablen "btn", der henviser til ".toggle-btn"
-const btn = document.querySelector(".toggle-btn");
-// Lav variablen "menu", der henviser til ".main-menu"
-const menu = document.querySelector(".main-menu");
-// Lav funktionen "toggleMenu()" med følgende funktionalitet
-function toggleMenu(){
 
 
-  // a) toggle klassen "shown" på menu vha. classList.toggle
-  menu.classList.toggle("shown")
-  // b) lav variablen "menuShown", som siger, at menu-variablen indeholder klassen "shown" via classList.contains("")
-const menuShown = menu.classList.contains("shown")
-  // c) spørg om "menuShown" i if-sætningen nedenfor (=> if (menuShown)), og udskift teksten
+const menu = document.querySelector(".menu");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger= document.querySelector(".hamburger");
+const closeIcon= document.querySelector(".closeIcon");
+const menuIcon = document.querySelector(".menuIcon");
 
-
-  if (menuShown) {
-    console.log(menuShown); // se i konsollen
-
-    // sæt btn.textContent til "Luk", hvis menuShown er "true"
+function toggleMenu() {
+  if (menu.classList.contains("showMenu")) {
+    menu.classList.remove("showMenu");
+    closeIcon.style.display = "none";
+    menuIcon.style.display = "block";
   } else {
-    console.log(menuShown); // se i konsollen
-    // sæt btn.textContent til "Menu", hvis menuShown er "false"
+    menu.classList.add("showMenu");
+    closeIcon.style.display = "block";
+    menuIcon.style.display = "none";
   }
-// "toggleMenu()" slutter her
 }
-// Tilføj et klik-event til "btn", der sætter toggleMenu-funktionen i gang
 
-btn.addEventListener("click" , toggleMenu)
+hamburger.addEventListener("click", toggleMenu);
+
+menuItems.forEach( 
+  function(menuItem) { 
+    menuItem.addEventListener("click", toggleMenu);
+  }
+)
